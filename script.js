9db6afc6681a619b4c0ca9cc06083f0916ddd61d
Gylmsry.online <112378723+specialman12@users.noreply.github.com>
2024-12-31 08:33:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyC8Ucsn_xUsYxBUjN7LWzcb6DDG9izQQ2U"; // Your API key here
+const API_KEY = "AIzaSyAW9ZJBBrZS5xEgtRXlV43deli4vPa-zJU"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
