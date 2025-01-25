fc4edf31e708af21e25d61a74b5c81b05e526a52
Gylmsry.online <112378723+specialman12@users.noreply.github.com>
2025-01-25 12:25:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyDwfVmTiVFVyfR8vm01qjkPtVqJP56cYl4"; // Your API key here
+const API_KEY = "AIzaSyAr9SRaGwIcrVCg5A88NIui780Lig6sqm0"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
